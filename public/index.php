84e43e9dce6e2b5ddfdc96aff57354b8b3ea875e
elisad5791 <elisad5791@mail.ru>
2022-10-11 15:21:40 +0500

    Add bootstrap

--- public/index.php
+++ public/index.php
@@ -3,12 +3,18 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $response->write('Welcome to Slim!');
+    $params = [];
+    return $this->get('renderer')->render($response, 'index.phtml', $params);
 });
 
 $app->run();
