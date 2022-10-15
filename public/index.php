5458f9b82a7b1dbd9fd66710c7a1830b0db01a80
elisad5791 <elisad5791@mail.ru>
2022-10-16 01:48:57 +0500

    Add db

--- public/index.php
+++ public/index.php
@@ -19,13 +19,50 @@ $app->get('/', function ($request, $response) {
 
 $app->post('/', function ($request, $response) {
     $url = $request->getParsedBodyParam('url');
-    $name = $url['name'];
+    $v = new Valitron\Validator($url);
+    $v->rule('required', 'name');
+    $v->rule('lengthMax', 'name', 255);
+    $v->rule('url', 'name');
+
+    if($v->validate()) {
+        $name = $url['name'];
+        $dsn = "pgsql:host=localhost;port=5432;dbname=elisad5791";
+        $db = new PDO($dsn, 'elisad5791', 'HigginS5791');
+        $sql = "INSERT INTO urls(name, created_at) VALUES (?, NOW())";
+        $query = $db->prepare($sql);
+        $query->execute([$name]);
+        $db = null;
+    }
+
+    return $response->withRedirect('/');
+});
+
+$app->get('/urls/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+
     $dsn = "pgsql:host=localhost;port=5432;dbname=elisad5791";
     $db = new PDO($dsn, 'elisad5791', 'HigginS5791');
-    $sql = "INSERT INTO urls(name, created_at) VALUES (?, NOW())";
-    $query = $db->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
-    $query->execute([$name]);
-    return $response->withRedirect('/');
+    $sql = "SELECT * FROM urls WHERE id=?";
+    $query = $db->prepare($sql);
+    $result = $query->execute([$id]);
+    $data = $query->fetch();
+    $db = null;
+    
+    $params = ['url' => $data];
+    return $this->get('renderer')->render($response, 'show.phtml', $params);
+});
+
+$app->get('/urls', function ($request, $response) {
+    $dsn = "pgsql:host=localhost;port=5432;dbname=elisad5791";
+    $db = new PDO($dsn, 'elisad5791', 'HigginS5791');
+    $sql = "SELECT * FROM urls";
+    $query = $db->prepare($sql);
+    $result = $query->execute();
+    $data = $query->fetchAll();
+    $db = null;
+    
+    $params = ['urls' => $data];
+    return $this->get('renderer')->render($response, 'urls.phtml', $params);
 });
 
 $app->run();
