c94be8a2c01100a2af2cb97139c59291f79e8a89
elisad5791 <elisad5791@mail.ru>
2022-10-23 16:06:44 +0500

    fix

--- public/index.php
+++ public/index.php
@@ -59,7 +59,8 @@ $app->post('/', function ($request, $response) use ($router, $db) {
         return $this->get('renderer')->render($response, 'index.phtml', $params);*/
         $route = $router->urlFor('urls');
         $this->get('flash')->addMessage('error', 'Некорректный URL');
-        return $response->withRedirect($route, 422);
+        $response2 = $response->withStatus(422);
+        return $response2->withRedirect($route);
     }
 
 
